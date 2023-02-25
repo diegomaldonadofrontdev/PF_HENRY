855773fc7fbe7872d573638fa35c901cabd2ca57
Diego Maldonado <108159490+diegomaldonadofrontdev@users.noreply.github.com>
2023-02-25 13:18:17 -0300

    Commit de prueba 2

--- client/src/components/MenuHome/MenuHome.jsx
+++ client/src/components/MenuHome/MenuHome.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./MenuHome.module.css";
+import img from "../../images/pizza.jpg";
 
 export default function MenuHome() {
 	return (
@@ -15,6 +16,18 @@ export default function MenuHome() {
 					<a href="a">Pastas</a>
 					<a href="a">Sanguches</a>
 					<a href="a">Bebidas</a>
+				</div>
+				<div className={styles.menu__cards}>
+					<div className={styles.menu__card}>
+						<div className={styles.fondo}>
+							<img src={img} alt="" />
+						</div>
+						<div className={styles.card__info}>
+							<h4>Pizza Italiana</h4>
+							<p>$100</p>
+							<p>Comprar</p>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
