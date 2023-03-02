b504e3c6d8a349ee269ddbe376e3bea66dc93c73
Diego Maldonado <108159490+diegomaldonadofrontdev@users.noreply.github.com>
2023-03-02 14:43:07 -0300

    mitad de la vista de registro owner
    contiene la mitad de la view del form del owner!

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import AdminOwner from "./views/AdminOwner/AdminOwner";
 import Home from "./views/home/Home";
+import HomeLocal from "./views/HomeLocal/HomeLocal";
 import Login from "./views/Login/Login";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
 				<Route path="/" element={<Home />} />
 				<Route path="/adminowner" element={<AdminOwner />} />
 				<Route path="/login" element={<Login />} />
+				<Route path="/s" element={<HomeLocal />} />
 			</Routes>
 		</div>
 	);
