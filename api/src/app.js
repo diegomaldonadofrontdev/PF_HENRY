35703a67229acdaa645b5ea502df1e4c7b94637c
RicardoFernandez <rreyesfdz96@gmail.com>
2023-03-02 10:14:01 -0600

    Routes and modified index, app, indexJs

--- api/src/app.js
+++ api/src/app.js
@@ -1,3 +1,11 @@
+const express = require('express');
+const server = express();
+const index = require('./Routes/Index')
+
+server.use(express.json());
+server.use('/',index);
+
+module.exports = server;
 
 /*
 CONFIGURACION DE EXPRESS
