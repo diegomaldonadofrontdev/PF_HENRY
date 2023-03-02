cf447ee1a40f40b0bdb6644645550750247000ba
ElQuebeck <115952174+ElQuebeck@users.noreply.github.com>
2023-03-02 17:06:43 -0300

    Add clientsRouter routes and any folder with any files models

--- api/src/Routes/tradesRouter.js
+++ api/src/Routes/tradesRouter.js
@@ -1,20 +1,20 @@
 const { Router } = require("express");
+const { createTradeHandler } = require("../handlers/tradesHandler");
 const {
   getProductsHandler,
   getProductHandler,
   createProductHandler,
   deleteProductHandler,
-  putProductHandler
+  putProductHandler,
 } = require("../handlers/productsHandler");
 const {
-        getUsersHandler,
-        getUserHandler,
-        createUserHandler,
-        deleteUserHandler,
-        putUserHandler
-} = require ("../Handlers/usersHandlers")
-const { validateProducts, validateUsers } = require("../middlewares/validate");
-
+  getUsersHandler,
+  getUserHandler,
+  createUserHandler,
+  deleteUserHandler,
+  putUserHandler,
+} = require("../Handlers/usersHandlers");
+const { validateProducts, validateUsers, validateTrades } = require("../middlewares/validate");
 
 const tradesRouter = Router();
 
@@ -23,9 +23,12 @@ tradesRouter.get("/products", getProductsHandler);
 tradesRouter.get("/products/:id", getProductHandler);
 tradesRouter.get("/users", getUsersHandler);
 tradesRouter.get("/users/:id", getUserHandler);
+tradesRouter.get("/orders", getOrdersHandler);
+tradesRouter.get("/orders/:id", getOrderHandler);
 tradesRouter.get("/membership", getMembershipHandler);
 
 // POST
+tradesRouter.post("/trades", validateTrades, createTradeHandler);
 tradesRouter.post("/users", validateUsers, createUserHandler);
 tradesRouter.post("/products", validateProducts, createProductHandler);
 
@@ -36,6 +39,5 @@ tradesRouter.delete("/products/:id", validateProducts, deleteProductHandler);
 // PUT
 tradesRouter.put("/users/:id", validateUsers, putUserHandler);
 tradesRouter.put("/products/:id", validateProducts, putProductHandler);
-
 
 module.exports = tradesRouter;
