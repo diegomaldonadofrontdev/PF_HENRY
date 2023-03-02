b4800414910d7993b069016dfd57763f80acb047
Diego Maldonado <108159490+diegomaldonadofrontdev@users.noreply.github.com>
2023-03-02 10:24:47 -0300

    Nuevo Footer e Input en en el buscador del Hero
    Se agrego un nuevo footer y el input del buscador en la seccion hero(la primera que aparece)

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,9 +1,33 @@
 import React from "react";
 import styles from "./Footer.module.css";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
 	return (
 		<footer className={styles.footer}>
+			<div className={styles.container}>
+				<div className={styles.faqs}>
+					<div>
+						<Link className={styles.faq}>Quienes somos?</Link>
+						<Link className={styles.faq}>Terminos y condiciones</Link>
+						<Link className={styles.faq}>Privacidad</Link>
+						<Link className={styles.faq}>Se parte de Pedi-Very</Link>
+						<Link className={styles.faq}>Blog</Link>
+					</div>
+					<div>
+						<Link className={styles.faq}>Top comidas</Link>
+						<Link className={styles.faq}>Top cadenas</Link>
+						<Link className={styles.faq}>Top ciudades</Link>
+					</div>
+					<div>
+						<Link className={styles.faq}>Registra tu negocio</Link>
+						<Link className={styles.faq}>Centro de Socios</Link>
+					</div>
+					<div>
+						<Link className={styles.faq}>Acuerdos corporativos</Link>
+					</div>
+				</div>
+			</div>
 			<div className={styles.container}>
 				<p>Desarrollado por PediVery</p>
 			</div>
--- client/src/components/HeroMain/HeroMain.jsx
+++ client/src/components/HeroMain/HeroMain.jsx
@@ -8,6 +8,13 @@ export default function HeroMain() {
 				<div>
 					<h2>Pedí lo que quieras!</h2>
 					<p>Restaurantes, mercados, farmacias, kioscos y mucho más</p>
+					<div className={styles.buscador}>
+						<p>Veamos que tenemos cerca</p>
+						<div className={styles.input}>
+							<i class="bx bx-map"></i>
+							<input type="text" placeholder="Dirección o referencia" />
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
