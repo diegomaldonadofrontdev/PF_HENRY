bb87439a82dbe51cf6e163d28c37e145aea00b62
Diego Andres Meneses <die.meneses@udla.edu.co>
2023-03-06 10:33:56 -0500

    jwt

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 import Navbar from "../NavBar/Navbar";
 import styles from "./Header.module.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import ButtonPrimary from "../ButtonPrimary/ButtonPrimary";
 
 export default function Header() {
+
+	const navigate = useNavigate();
+
+	const logout = () => {
+		window.localStorage.setItem(
+			'loggedNoteAppUser', JSON.stringify(null)
+		)
+		navigate("/login");
+	}
+
 	return (
 		<header className={styles.header}>
 			<div className={styles.container}>
@@ -12,6 +23,8 @@ export default function Header() {
 				</div>
 
 				<Navbar />
+
+				<button onClick={logout}>Logout</button>
 			</div>
 		</header>
 	);
