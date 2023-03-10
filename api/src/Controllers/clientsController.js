a44254decebef4b46d2f015fd4824253ef8d7383
RicardoFernandez <rreyesfdz96@gmail.com>
2023-03-09 22:14:58 -0600

    Resolve post and send email

--- api/src/Controllers/clientsController.js
+++ api/src/Controllers/clientsController.js
@@ -1,6 +1,7 @@
 const feedbacks = [];
 const Clients = require('../models/Clients');
 const Order = require('../models/Order')
+const sendMail = require('../Helpers/email')
 
 
 const bcrypt = require('bcryptjs');
@@ -28,15 +29,24 @@ const getFeedbacksController = () => {
 const postCreateClientController = async (body) => {
 
   const { password } = body
-  console.log(password);
   
-  newClient = new Clients( body);
+  try {
+    newClient = new Clients( body);
   
-  const salt = bcrypt.genSaltSync(10);
-  newClient.password = bcrypt.hashSync(password,salt)
-  
-  await newClient.save();
-  return true;
+    const salt = bcrypt.genSaltSync(10);
+    newClient.password = bcrypt.hashSync(password,salt)
+    
+    await newClient.save();
+    
+    sendMail("Bienvenido, gracias por registrarte");
+    
+      
+  } catch (error) {
+    return false
+  }
+
+
+  //return true;
 }
 
 const postCreateOrder = async (body) => {
