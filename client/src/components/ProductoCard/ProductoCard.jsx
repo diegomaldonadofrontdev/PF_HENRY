7d6646ea9e505f2420a87fabce126dfb4593c569
Diego Maldonado <108159490+diegomaldonadofrontdev@users.noreply.github.com>
2023-03-14 14:23:55 -0300

    estilos

--- client/src/components/ProductoCard/ProductoCard.jsx
+++ client/src/components/ProductoCard/ProductoCard.jsx
@@ -24,16 +24,18 @@ export default function ProductoCard(props) {
 
 	return (
 		<div className={styles.product__card}>
-			<div className={styles.producto__info}>
-				<h3>{props.name}</h3>
-				<p className={styles.description}> {props.description}</p>
-				<p className={styles.price}>${props.price}</p>
+			<div>
+				<div className={styles.img__container}>
+					<img src={props.img} alt="" />
+				</div>
+				<div className={styles.producto__info}>
+					<h3>{props.name}</h3>
+					<p className={styles.description}> {props.description}</p>
+					<p className={styles.price}>${props.price}</p>
+				</div>
+			</div>
 
-				<ButtonProduct fc={agregarCarrito} />
-			</div>
-			<div className={styles.img__container}>
-				<img src={props.img} alt="" />
-			</div>
+			<ButtonProduct fc={agregarCarrito} />
 		</div>
 	);
 }
