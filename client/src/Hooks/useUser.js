fc6baf80b93a21111ddabc063b527e9d3de8fd0d
Diego Andres Meneses <die.meneses@udla.edu.co>
2023-03-15 16:09:41 -0500

    z

--- client/src/Hooks/useUser.js
+++ client/src/Hooks/useUser.js
@@ -11,37 +11,31 @@ export default function useUser() {
 
     const dispatch = useDispatch();
     const { token, setToken } = useContext(Context);
-    const [state, setState] = useState({ loading: false, error: false });
     const { user } = useAuth0();
 
     const login = useCallback(({ email, password }) => {
-        setState({ loading: true, error: false })
+
         loginService({ email, password })
             .then(data => {
                 window.localStorage.setItem('token', JSON.stringify(data[1].token))
-                setState({ loading: false, error: false })
                 setToken(data[1].token)
                 dispatch(currentCLient(data[0]))
             })
             .catch(err => {
                 window.localStorage.removeItem('token')
-                setState({ loading: false, error: true })
                 console.log(err)
             })
     }, [setToken, dispatch])
 
     const sigin = useCallback((user) => {
-        setState({ loading: true, error: false })
         registerService(user)
             .then(data => {
                 window.localStorage.setItem('token', JSON.stringify(data[1].token))
-                setState({ loading: false, error: false })
                 setToken(data[1].token)
                 dispatch(currentCLient(data[0]))
             })
             .catch(err => {
                 window.localStorage.removeItem('token')
-                setState({ loading: false, error: true })
                 console.log(err)
             })
     }, [setToken, dispatch]);
@@ -68,8 +62,6 @@ export default function useUser() {
 
     return {
         isLogged: Boolean(token),
-        isLoginLoading: state.loading,
-        hasLoginError: state.error,
         login,
         logout1,
         sigin,
--- client/src/views/RegistrationForm/RegistrationForm.jsx
+++ client/src/views/RegistrationForm/RegistrationForm.jsx
@@ -8,7 +8,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 export default function RegistrationForm() {
 
-	const { isLoginLoading, hasLoginError, sigin, isLogged } = useUser();
+	const { sigin, isLogged } = useUser();
 	const navigate = useNavigate();
 	const { isAuthenticated } = useAuth0();
 
@@ -44,60 +44,56 @@ export default function RegistrationForm() {
 
 	return (
 		<>
-			{isLoginLoading && <strong>Checking credentials...</strong>}
-			{!isLoginLoading && (
-				<div className={styles.registration__form}>
-					<Header />
-					<div className={styles.container}>
-						<form onSubmit={handleRegister} action="" className={styles.form}>
-							<div className={styles.twoColumns}>
-								<div className={styles.input__container}>
-									<label htmlFor="">Nombre</label>
-									<input type="text" placeholder="" name="firstname" value={user.firstname} onChange={handleChange} />
-								</div>
-								<div className={styles.input__container}>
-									<label htmlFor="">Apellido</label>
-									<input type="text" placeholder="" name="lastname" value={user.lastname} onChange={handleChange} />
-								</div>
+			<div className={styles.registration__form}>
+				<Header />
+				<div className={styles.container}>
+					<form onSubmit={handleRegister} action="" className={styles.form}>
+						<div className={styles.twoColumns}>
+							<div className={styles.input__container}>
+								<label htmlFor="">Nombre</label>
+								<input type="text" placeholder="" name="firstname" value={user.firstname} onChange={handleChange} />
 							</div>
-							<div className={styles.twoColumns}>
-								<div className={styles.input__container}>
-									<label htmlFor="">Email</label>
-									<input type="text" placeholder="" name="email" value={user.email} onChange={handleChange} />
-								</div>
-								<div className={styles.input__container}>
-									<label htmlFor="">Password</label>
-									<input type="password" placeholder="" name="password" value={user.password} onChange={handleChange} />
-								</div>
+							<div className={styles.input__container}>
+								<label htmlFor="">Apellido</label>
+								<input type="text" placeholder="" name="lastname" value={user.lastname} onChange={handleChange} />
 							</div>
-							<div className={styles.twoColumns}>
-								<div className={styles.input__container}>
-									<label htmlFor="">País</label>
-									<input type="text" placeholder="" name="country" value={user.country} onChange={handleChange} />
-								</div>
-								<div className={styles.input__container}>
-									<label htmlFor="">Ciudad</label>
-									<input type="text" placeholder="" name="city" value={user.city} onChange={handleChange} />
-								</div>
-								<div className={styles.input__container}>
-									<label htmlFor="">Dirección</label>
-									<input type="text" placeholder="" name="address" value={user.address} onChange={handleChange} />
-								</div>
+						</div>
+						<div className={styles.twoColumns}>
+							<div className={styles.input__container}>
+								<label htmlFor="">Email</label>
+								<input type="text" placeholder="" name="email" value={user.email} onChange={handleChange} />
 							</div>
+							<div className={styles.input__container}>
+								<label htmlFor="">Password</label>
+								<input type="password" placeholder="" name="password" value={user.password} onChange={handleChange} />
+							</div>
+						</div>
+						<div className={styles.twoColumns}>
+							<div className={styles.input__container}>
+								<label htmlFor="">País</label>
+								<input type="text" placeholder="" name="country" value={user.country} onChange={handleChange} />
+							</div>
+							<div className={styles.input__container}>
+								<label htmlFor="">Ciudad</label>
+								<input type="text" placeholder="" name="city" value={user.city} onChange={handleChange} />
+							</div>
+							<div className={styles.input__container}>
+								<label htmlFor="">Dirección</label>
+								<input type="text" placeholder="" name="address" value={user.address} onChange={handleChange} />
+							</div>
+						</div>
 
-							<div className={styles.twoColumns}>
-								<div className={styles.input__container}>
-									<label htmlFor="">Teléfono</label>
-									<input type="text" placeholder="" name="phone" value={user.phone} onChange={handleChange} />
-								</div>
+						<div className={styles.twoColumns}>
+							<div className={styles.input__container}>
+								<label htmlFor="">Teléfono</label>
+								<input type="text" placeholder="" name="phone" value={user.phone} onChange={handleChange} />
 							</div>
+						</div>
 
-							<button style={{ border: "none" }}><ButtonPrimary texto="CREAR CUENTA" /></button>
-						</form>
-					</div>
+						<button style={{ border: "none" }}><ButtonPrimary texto="CREAR CUENTA" /></button>
+					</form>
 				</div>
-			)}
-			{hasLoginError && <strong>Credentials are invalid!</strong>}
+			</div>
 		</>
 	);
 
