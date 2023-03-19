62af8bbd4d93cc38be852d3d6baf2e473d8e5f98
RicardoFernandez <rreyesfdz96@gmail.com>
2023-03-19 11:19:46 -0600

    Update routes

--- api/src/Helpers/emailResetPasswordSuccesTrade.js
+++ api/src/Helpers/emailResetPasswordSuccesTrade.js
@@ -43,7 +43,7 @@ const sendMailResetSuccessTrade = async(email, username) => {
                                             <td style="padding:0 0 36px 0;color:#153643;">
                                                 <h1 style="font-size:24px;margin:0 0 20px 0;color:#ff441f">Hola ${username}</h1>
                                                 <p style="margin:0 0 12px 0;font-size:16px;line-height:24px;">Se ha restablecido tu contraseña.</p>
-                                                <p style="margin:0;font-size:16px;line-height:24px"><a href="" style="color:#ff441f;text-decoration:underline;">Ingresa ahora</a></p>
+                                                <p style="margin:0;font-size:16px;line-height:24px"><a href="pedivery.netlify.app/login/tradeboss" style="color:#ff441f;text-decoration:underline;">Ingresa ahora</a></p>
                                             </td>
                                         </tr>
                                     </table>
