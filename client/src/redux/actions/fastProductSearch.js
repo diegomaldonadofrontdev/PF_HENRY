11abab6df7167b0605f3c5db83e23f0a7e922ec9
Diego Maldonado <108159490+diegomaldonadofrontdev@users.noreply.github.com>
2023-03-21 14:23:17 -0300

    super admin inicio

--- client/src/redux/actions/fastProductSearch.js
+++ client/src/redux/actions/fastProductSearch.js
@@ -1,7 +1,6 @@
 import { GET_PRODUCT } from "../actions/types";
 
 export function fastProductSearch(name) {
-	console.log("en la action " + name);
 	return async function (dispatch) {
 		return dispatch({
 			type: GET_PRODUCT,
--- client/src/views/SuperAdmin/SuperAdmin.jsx
+++ client/src/views/SuperAdmin/SuperAdmin.jsx
@@ -2,14 +2,19 @@ import React, { useEffect } from "react";
 import styles from "./SuperAdmin.module.css";
 import Header from "../../components/Header/Header";
 import { getTradesByName } from "../../redux/actions/getTradesByName";
+import { getTrades } from "../../redux/actions/getTrades";
+
 import { useDispatch, useSelector } from "react-redux";
 
 export default function SuperAdmin() {
 	const dispatch = useDispatch();
-
-	const allCommerces = useSelector((state) => state.allCommerces);
+	const commerces = useSelector((state) => state.filterCommerce);
+	useEffect(() => {
+		dispatch(getTrades());
+	}, []);
 
 	function handlerFilterByName(e) {
+		console.log(e.target.value);
 		dispatch(getTradesByName(e.target.value));
 	}
 
@@ -50,10 +55,12 @@ export default function SuperAdmin() {
 								<input type="text" onChange={handlerFilterByName} />
 							</form>
 							<div className={styles.trades__container}>
-								<div className={styles.sp_tradeCard}>
-									<h4>Nombre</h4>
-									<i class="bx bx-trash"></i>
-								</div>
+								{commerces.map((x) => (
+									<div className={styles.sp_tradeCard}>
+										<h4>{x.commerceName}</h4>
+										<i class="bx bx-trash"></i>
+									</div>
+								))}
 							</div>
 						</div>
 						<div>
@@ -124,26 +131,85 @@ export default function SuperAdmin() {
 				<div className={styles.content}>
 					<h3>Pedidos</h3>
 					<div className={styles.grid}>
+						<div className={styles.dosColumnas}>
+							<div>
+								<h4>Buscar pedido por nro de orden</h4>
+								<input type="text" />
+								<input type="submit" />
+							</div>
+
+							<form action="">
+								<p>Filrar por comercio</p>
+								<select name="" id="">
+									<option value="">Comercio</option>
+								</select>
+							</form>
+						</div>
 						<div>
-							<h4>Buscar pedido por nro de orden</h4>
-							<input type="text" />
-							<input type="submit" />
-							<select name="" id="">
-								<option value="">Comercio</option>
-							</select>
+							<h4>Resultados</h4>
+						</div>
+						<div className={styles.containerReview}>
+							<h3>Datos del usuario</h3>
 						</div>
 					</div>
 				</div>
 				<div className={styles.content}>
 					<h3>Reviews</h3>
-					<div>
-						<div></div>
+					<div className={styles.grid}>
+						<div>
+							<h4>Ver reviews</h4>
+							<table>
+								<tr>
+									<th>Usuario</th>
+									<th>Review</th>
+									<th>Accion</th>
+								</tr>
+								<tr>
+									<td>John</td>
+									<td>Doe</td>
+									<td>
+										<button>Eliminar</button>
+									</td>
+								</tr>
+							</table>
+						</div>
+
+						<div className={styles.containerReview}>
+							<h3>Resultados Reviews</h3>
+						</div>
 					</div>
 				</div>
 				<div className={styles.content}>
 					<h3>Clientes</h3>
-					<div>
-						<div></div>
+					<div className={styles.grid}>
+						<div>
+							<h4>Clientes Registrados</h4>
+							<table>
+								<tr>
+									<th>Usuario</th>
+									<th>Review</th>
+									<th>Accion</th>
+								</tr>
+								<tr>
+									<td>John</td>
+									<td>Doe</td>
+									<td>
+										<button>Eliminar</button>
+									</td>
+								</tr>
+							</table>
+						</div>
+						<div>
+							<h3>Datos del usuario</h3>
+							<h5>Editar</h5>
+							<form action="">
+								<input type="text" />
+								<input type="text" />
+								<input type="text" />
+								<input type="text" />
+								<input type="submit" value={'cambiar'} />
+							</form>
+						</div>
 					</div>
 				</div>
 			</div>
